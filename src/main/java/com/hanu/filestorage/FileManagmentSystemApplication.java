372147de0667594061af666d664bd887771bc528
Bang <nhbang@IfiSolution.local>
2021-12-08 16:58:38 +0700

    Init File management project

---
+++ src/main/java/com/hanu/filestorage/FileManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hanu.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagmentSystemApplication.class, args);
+	}
+
+}
