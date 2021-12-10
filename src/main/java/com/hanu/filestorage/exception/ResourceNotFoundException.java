acdb04b88204619cce2ff922bd58bad0f548c5be
Bang <nhbang@IfiSolution.local>
2021-12-10 17:52:00 +0700

    Error handling

--- src/main/java/com/hanu/filestorage/exception/ResourceNotFoundException.java
+++ src/main/java/com/hanu/filestorage/exception/ResourceNotFoundException.java
@@ -1,4 +1,9 @@
 package com.hanu.filestorage.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
 }
